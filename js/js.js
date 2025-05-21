21b441969ad53b2ab7d18830859bcd193e279932
prembalchandra <143958363+prembalchandra@users.noreply.github.com>
2025-05-21 10:22:42 +0530

    Update js.js

--- js/js.js
+++ js/js.js
@@ -1,11 +1,27 @@
-document.getElementById('myForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-  
-    var name = document.getElementById('name').value;
-    var email = document.getElementById('email').value;
-  
-    console.log('Name:', name);
-    console.log('Email:', email);
-  
-  });
-  
+;(function($) {
+
+    $(document).ready( function() {
+        $(document).on('click', '.header-area .show-menu', function() {
+            $(this).toggleClass('active');
+            $(".header-area .navbar").toggleClass('active');
+        });
+        // $(document).on('click', '.header-area .navbar .close-menu', function() {
+        //     $(".header-area .navbar").removeClass('active');
+        // });
+
+        AOS.init({
+            duration: 1500,
+            once: true,
+        })
+    });
+
+})(jQuery);
+
+
+var div = document.createElement("div");
+    div.id="preloader",
+    div.className="preloader",
+    div.innerHTML='<div class="black_wall"></div><div class="loader"></div>',
+    document.body.insertBefore(div,document.body.firstChild),window.onload=function() {
+    document.getElementById("preloader").classList.add("off")
+};
