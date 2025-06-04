d34d810b70b7d4c2354145aedb77ce9fd7c1994e
Baibhav <baibhavtiwari@hydear.com>
2025-06-04 22:43:53 +0530

    style-chnages

--- js/js.js
+++ js/js.js
@@ -25,3 +25,23 @@ var div = document.createElement("div");
     document.body.insertBefore(div,document.body.firstChild),window.onload=function() {
     document.getElementById("preloader").classList.add("off")
 };
+
+$(document).ready(function () {
+    const $menuToggler = $(".header_headerMenuToggler__UpQ2o");
+    const $menu = $(".header_headerWrapper__Menu__qjnVS");
+    const $hamburger = $(".header_hamburger__u8Soo");
+
+    $menuToggler.on("click", function () {
+      // Toggle open/close menu class
+      $menu.toggleClass("header_openMenu__1fCi7");
+      $hamburger.toggleClass("header_is_active__MjysY");
+    });
+
+    // Optional: close menu when clicking outside
+    $(document).on("click", function (e) {
+      if (!$(e.target).closest(".header_headerWrapper__Menu__qjnVS, .header_headerMenuToggler__UpQ2o").length) {
+        $menu.removeClass("header_openMenu__1fCi7");
+        $hamburger.removeClass("header_is_active__MjysY");
+      }
+    });
+  });
